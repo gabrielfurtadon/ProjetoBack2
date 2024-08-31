73ec3f5c25f079b43105333d359e144e267ff61a
gabrielfurtadon <gabrielfneves2001@gmail.com>
2024-08-30 22:11:57 -0300

    atualizacoes da apresentacao

--- controller/InvestmentAccountController.js
+++ controller/InvestmentAccountController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const InvestmentAccountService = require('../service/InvestmentAccountService');
 const { validaAcesso } = require("../helpers/Auth.js");
+const UserService = require('../service/UserService')
 
 router.post('/create/:codigo', validaAcesso,  async (req, res) => {
     const { codigo } = req.params; 
@@ -29,10 +30,20 @@ router.get('/balance/:codigo', validaAcesso, async (req, res) => {
 // Visualizar histórico de transações
 router.get('/transactions/:codigo', validaAcesso, async (req, res) => {
     const { codigo } = req.params;
+    const adminUser = await UserService.getById(req.user.codigo)
 
     try {
-        const transactions = await InvestmentAccountService.getTransactionHistory(codigo);
-        res.status(200).json(transactions);
+        if(adminUser.isAdmin == true) {
+            const transactions = await InvestmentAccountService.getTransactionHistory(codigo);
+            res.status(200).json(transactions);
+        }else {
+           if (adminUser.codigo == req.params.codigo) {
+                const transactions = await InvestmentAccountService.getTransactionHistory(codigo);
+                res.status(200).json(transactions);
+           }else {
+            res.status(404).json({ message: "Voce nâo pode ver as transaçôes desse usuário"});
+           }
+        }
     } catch (error) {
         res.status(500).json({ message: "Erro ao visualizar histórico de transações.", error: error.message });
     }
